827ad1d5a79b022424014f6a81d77dc5271a7b60
magna <magna@LAPTOP-T588JKLS>
2021-06-14 23:52:36 -0500

    Añadiendo categoria y dato

--- src/java/com/controlador/controlador.java
+++ src/java/com/controlador/controlador.java
@@ -45,7 +45,8 @@ public class controlador extends HttpServlet {
                             /*D:\escritorio-nexus\Universidad\sistemas inteligentes\nuevo\proyecto_final\web\imagenes*/
                             // File f = new File("D:\\escritorio-nexus\\Universidad\\sistemas inteligentes\\nuevo\\proyecto_final\\web\\imagenes\\productos\\" + fileItem.getName());
                             //   File f = new File("D:\\escritorio-nexus\\Universidad\\diseño web\\proyecto_final\\web\\imagenes\\productos\\" + fileItem.getName());
-                            File f = new File("C:\\Sowad\\proyecto_final\\web\\imagenes\\productos\\" + fileItem.getName());
+                            //File f = new File("C:\\Sowad\\proyecto_final\\web\\imagenes\\productos\\" + fileItem.getName());
+                            File f = new File("C:\\Users\\magna\\Dropbox\\Mi PC (LAPTOP-T588JKLS)\\Desktop\\Nueva Carpeta (5)\\" + fileItem.getName());
                             fileItem.write(f);
                             p.setProducto_imagen(f.getAbsolutePath());
                         } else {
